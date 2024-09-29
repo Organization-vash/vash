bd78fb69cd98279793fcb9a25b5802ebd1e020a6
Sergio <svelasquezr1@gmail.com>
2024-09-29 11:49:46 -0500

    feat(backend) se implementó el acceso a otros puertos desde el controlador

--- src/main/java/com/vash/entel/api/AuthController.java
+++ src/main/java/com/vash/entel/api/AuthController.java
@@ -1,19 +1,16 @@
 package com.vash.entel.api;
 
-import com.vash.entel.dto.UserDTO;
 import com.vash.entel.model.entity.User;
 import com.vash.entel.service.impl.UserServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 public class AuthController {
     private final UserServiceImpl userService;
 
