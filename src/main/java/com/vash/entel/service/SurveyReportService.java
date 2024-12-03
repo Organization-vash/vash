533146f49835ca90ff7031f102f3bbdcc8ede0aa
VaniaR <vramosc2@upao.edu.pe>
2024-12-03 01:38:17 -0500

    survey-report-complete

--- src/main/java/com/vash/entel/service/SurveyReportService.java
+++ src/main/java/com/vash/entel/service/SurveyReportService.java
@@ -1,8 +1,13 @@
 package com.vash.entel.service;
+import org.jfree.chart.JFreeChart;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public interface SurveyReportService {
     List<Map<String, Object>> getSurveyReport();
+    byte[] generatePieChart() throws IOException;
+    byte[] generateBarChart() throws IOException;
+    byte[] generateExcelReport() throws IOException;
 }
